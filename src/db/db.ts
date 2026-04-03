6095b62b916c046aaeb6b04c57053d8004ab14ea
bebekdas7 <bebekdas7@gmail.com>
2026-04-03 19:12:09 +0530

    fix(db): Enabled ssl

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
-  ssl: false,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
