3fa45ce76698254523cb64cdd01af6b0d3a9d2a4
Bebek Das <bebek.das@necsws.com>
2026-07-11 12:36:11 +0530

    fix(db): Update SSL configuration for production environment

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,7 +6,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl:
+  process.env.NODE_ENV === "production" &&
+  process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 export default pool;
